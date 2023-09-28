0aac4686cabc09bb54fded777710294151b9f9a3
dzaki1971 <dzaki1971@gmail.com>
2023-09-28 09:12:10 +0700

    h

---
+++ anykernel.sh
@@ -0,0 +1,59 @@
+### AnyKernel3 Ramdisk Mod Script
+## osm0sis @ xda-developers
+
+### AnyKernel setup
+# begin properties
+properties() { '
+kernel.string=STRIX kernel by fiqri19102002 @ github
+do.devicecheck=1
+do.modules=0
+do.systemless=1
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=mojito
+device.name2=sunny
+supported.versions=11-13
+supported.patchlevels=
+'; } # end properties
+
+### AnyKernel install
+# begin attributes
+attributes() {
+set_perm_recursive 0 0 755 644 $ramdisk/*;
+set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
+} # end attributes
+
+
+## boot shell variables
+block=/dev/block/bootdevice/by-name/boot;
+is_slot_device=1;
+ramdisk_compression=auto;
+patch_vbmeta_flag=auto;
+
+# import functions/variables and setup patching - see for reference (DO NOT REMOVE)
+. tools/ak3-core.sh && attributes;
+
+## AnyKernel boot install
+dump_boot;
+
+# begin ramdisk changes
+
+# end ramdisk changes
+
+write_boot;
+## end boot install
+
+## vendor_boot shell variables
+block=vendor_boot;
+is_slot_device=1;
+ramdisk_compression=auto;
+patch_vbmeta_flag=auto;
+
+# reset for vendor_boot patching
+reset_ak;
+
+## AnyKernel vendor_boot install
+split_boot; # skip unpack/repack ramdisk since we don't need vendor_ramdisk access
+
+flash_boot;
+## end vendor_boot install
